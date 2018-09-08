7566d8a033d70ffa90a2e9f55e5f0a90b2f4cb39
Jan Brodhaecker <jan.brodhaecker@sovanta.com>
2018-09-08 17:40:40 +0200

    [ADD] adding documentation for random forest implementaiton

--- Implementierung Random Forest/main.py
+++ Implementierung Random Forest/main.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
@@ -8,10 +9,11 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 # Files Paths
-TEST_DATA_CSV = './Wohnungskartei/Wohnungskartei_Muster_Master_4_S.csv'
-TRAIN_DATA_CSV = './Wohnungskartei/Wohnungskartei_Muster_Master_6_S_teach.csv'
+TEST_DATA_CSV = os.path.join(os.getcwd(), 'Wohnungskartei', 'Wohnungskartei_Muster_Master_4_S.csv')
+TRAIN_DATA_CSV = os.path.join(os.getcwd(), 'Wohnungskartei', 'Wohnungskartei_Muster_Master_6_S_teach.csv')
 
 # Constants 
+MAX_RANDOM_TRESS = 1000
 DEFAULT_SEPEARTOR = ";"
 ATTRIBUTES = ["Zimmerzahl", "Stockwerk", "Heizung", "Hausmeister", "Kindergarten", "Schule", "S-Bahn", "Garage", "Miete", "Nebenkosten", "Alter", "Aufzug", "Lage", "Entfernung", "Kaution", "Kueche", "Bad", "Balkon", "Terrasse", "Kehrwoche", "Moebliert", "Quadratmeter"]
 LABEL_COLUMN = 'S'
@@ -39,17 +41,17 @@ x_test = STANDARD_SCALER.transform(testData['x'])
 
 # generate RandomForestClassifier
 best_accuracy = 0
-for x in range (1, 10000):
-    classifier = RandomForestClassifier(n_estimators=x, random_state=0)
-    classifier.fit(x_train, trainingData['y'])
-    y_pred = classifier.predict(x_test)
+# for x in range (1, MAX_RANDOM_TRESS):
+classifier = RandomForestClassifier(n_estimators=827, random_state=0)
+classifier.fit(x_train, trainingData['y'])
+y_pred = classifier.predict(x_test)
 
-    current_accuracy = accuracy_score(testData['y'], y_pred)
-    if (current_accuracy > best_accuracy):
-        print "Found best accuracy: " + str(current_accuracy) + "! With number of trees: " + str(x) + "."
-        best_accuracy = current_accuracy
+current_accuracy = accuracy_score(testData['y'], y_pred)
+#if (current_accuracy > best_accuracy):
+ #   print "Found best accuracy: " + str(current_accuracy) + "! With number of trees: " + str(x) + "."
+  #  best_accuracy = current_accuracy
 
 # For furher result information
-#print(confusion_matrix(testData['y'],y_pred))  
-#print(classification_report(testData['y'],y_pred))  
-#print(accuracy_score(testData['y'], y_pred))  
+print(confusion_matrix(testData['y'],y_pred))  
+print(classification_report(testData['y'],y_pred))  
+print(accuracy_score(testData['y'], y_pred))  
